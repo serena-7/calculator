52f667140303b4e0fd8edf3a567c9ceb7306ce4f
Serena Sorensen <serena.sorensen7@gmail.com>
2022-03-22 15:05:42 -0600

    Calculator Functioning

--- calc.js
+++ calc.js
@@ -14,7 +14,25 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	// console.log("mathSymbol: ", mathSymbol);
+	// console.log("num1: ", num1);
+	// console.log("num2: ", num2);
 
+	if (mathSymbol === '+'){
+		answer = num1 + num2;
+	} else if (mathSymbol === '-'){
+		answer = num1 - num2;
+	} else if (mathSymbol === '*'){
+		answer = num1 * num2;
+	} else if (mathSymbol === '/'){
+		answer = num1 / num2;
+	} else if (mathSymbol === 'sqrt'){
+		answer = Math.sqrt(num1);
+	} else{
+		answer = "invalid";
+	}
+
+	console.log("answer: ", answer);
 	// This line closes the connection to the command line interface.
 	reader.close()
 
