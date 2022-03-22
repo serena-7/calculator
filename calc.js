4cd712603e514c662e7662ec794a3f063fa8d4ae
Serena Sorensen <serena.sorensen7@gmail.com>
2022-03-22 16:05:27 -0600

    Can now handle 3 numbers

--- calc.js
+++ calc.js
@@ -13,19 +13,37 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	num3 = Number(tokens[3]);
 
-	// console.log("mathSymbol: ", mathSymbol);
-	// console.log("num1: ", num1);
-	// console.log("num2: ", num2);
+	console.log("mathSymbol: ", mathSymbol);
+	console.log("num1: ", num1);
+	console.log("num2: ", num2);
+	console.log("num3: ", num3);
 
 	if (mathSymbol === '+'){
-		answer = num1 + num2;
+		if(isNaN(num3)){
+			answer = num1 + num2;
+		} else {
+			answer = num1 + num2 + num3;
+		}
 	} else if (mathSymbol === '-'){
-		answer = num1 - num2;
+		if(isNaN(num3)){
+			answer = num1 - num2;
+		} else {
+		answer = num1 - num2 - num3;
+		}
 	} else if (mathSymbol === '*'){
-		answer = num1 * num2;
+		if(isNaN(num3)){
+			answer = num1 * num2;
+		} else {
+		answer = num1 * num2 * num3;
+		}
 	} else if (mathSymbol === '/'){
-		answer = num1 / num2;
+		if(nisNaN(num3)){
+			answer = num1 / num2;
+		} else {
+		answer = num1 / num2 / num3;
+		}
 	} else if (mathSymbol === 'sqrt'){
 		answer = Math.sqrt(num1);
 	} else if (mathSymbol === 'sq'){
